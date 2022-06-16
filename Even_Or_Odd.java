aae0c9b5f7f17a482b2c123fa739dd204f01d1c3
Cheyenne <95506830+CheyenneBarton@users.noreply.github.com>
2022-06-16 12:10:35 -0400

    Add files via upload

---
+++ Even_Or_Odd.java
@@ -0,0 +1,21 @@
+package SimpleProjects;
+
+import java.util.Scanner;
+
+public class Even_Or_Odd {
+
+	public static void main(String[] args) 
+	{
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.print("Enter a number: ");
+		int num = reader.nextInt();
+		
+		if (num % 2 ==0)
+			System.out.println(num + " is even");
+		
+		else
+			System.out.println(num + " is odd");
+	}
+
+}
